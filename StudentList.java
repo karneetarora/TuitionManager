2830196bbe63efe1788522e5e732a35cb8eb4936
Karneet Arora <44528134+karneetarora@users.noreply.github.com>
2020-02-20 10:34:33 -0500

    Update StudentList.java

--- StudentList.java
+++ StudentList.java
@@ -1,4 +1,26 @@
-
 public class StudentList {
-
+	
+	
+	/*
+	 * This method adds a student to the Student List
+	 * @param student being added to the list
+	 */
+	public void add(Student s) {
+		
+	}
+	
+	/**
+	 * This method removes a student from the Student List
+	 * @param student being removed from the list
+	 */
+	public void remove(Student s) {
+		
+	}
+	
+	/**
+	 * This method prints the Student List 
+	 */
+	public void print() {
+		
+	}
 }
