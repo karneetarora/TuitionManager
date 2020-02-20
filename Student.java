94010b03827e054959348079ddb87978c9c64882
Karneet Arora <44528134+karneetarora@users.noreply.github.com>
2020-02-20 10:33:01 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,4 +1,3 @@
-
 public abstract class Student implements Comparable {
 	 private String fname;    
 	 private String lname;    
@@ -6,18 +5,25 @@ public abstract class Student implements Comparable {
 	 
 	//constructor 
 	 public Student(String fname, String lname, int credit) {
-
+		 this.fname = fname; 
+		 this.lname = lname; 
+		 this.credit = credit; 
 	 }       
 	
 	 //must implement compareTo method because Student class implements the Comparable Interface    
 	 //return 0 if fname and lname are equal, -1 if this < obj, 1 if this > obj    
 	 public int compareTo(Object obj) {
-		 
+		 if(obj instanceof Student) {
+			 Student temp = (Student) obj; 
+			 if (temp.fname == this.lname)
+					 return 0; 	
+		 }
+		 return -1; 
 	 }
 	
 	 //return a string with fname, lname and credit hours; subclasses will be using this method.    
 	 public String toString() {
-		 
+		 return fname + " " + lname + " " + credit; 
 	 }     
 	   
 	 /**
