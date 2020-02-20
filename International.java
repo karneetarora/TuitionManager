1a615ee90465bcbab8c01eaec9fc81f8f9f90ada
Karneet Arora <44528134+karneetarora@users.noreply.github.com>
2020-02-20 10:30:54 -0500

    Update International.java

--- International.java
+++ International.java
@@ -1,3 +1,34 @@
-public class International extends Student {    
-	private boolean exchange;    
+public class International extends Student {
+	private boolean exchange; 
+	
+	public International(String fname, String lname, int credit, boolean isExchange) {
+		super(fname, lname, credit); 
+		exchange = isExchange; 
+	}
+	
+	public int tuitionDue() { 
+		int tuitionDue; 
+		if(credit < 12) {
+			if(exchange)
+				tuitionDue = 846 + 350; 
+			else
+				tuitionDue = 846 + 350 + 945 * credit; 
+		}
+		else {
+			if(exchange)
+				tuitionDue = 1441 + 350; 
+			else
+				tuitionDue = 1441 + 350 +  945 * credit ; 
+		}
+		return tuitionDue; 
+	}
+
 }
+
+/**
+ * Tuition - $945/credit 
+ * University Fee - $846 (part time) if credits less than 12
+ * University Fee - $1,441 (full time) if credits greater than 12
+ * International fee - $350
+ * Exchange students only pay university + international fee
+ */
