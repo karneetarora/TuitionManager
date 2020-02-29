406ce4f0f654f8d595237e8fe77c7c3b1d05e38e
Karneet Arora <44528134+karneetarora@users.noreply.github.com>
2020-02-29 15:31:08 -0500

    Update Outstate.java

--- Outstate.java
+++ Outstate.java
@@ -1,34 +1,95 @@
+ /**
+ * Outstate class is a subclass of Student Class which is used for Outstate Students
+ * Calculates the tuition amount and returns all information as a String
+ * Methods include - toString(), and tuitionDue()
+ * @author Karneet Arora, Manel Bermad
+ *
+ */
 public class Outstate extends Student {
 	private boolean tristate; 
 	
+	
 	/**
-	 * This is a constructor for Outstate students 
-	 * 
-	 * @param 
+	 * Constructor for Outstate students 
+	 * @param fname, lname, credit, isTriState
 	 */
 	public Outstate(String fname, String lname, int credit, boolean isTriState) {		
 		super(fname, lname, credit);
 		this.tristate = isTriState; 
 	}
 	
+	
 	 /**
-	  * This method computes the total tuition amount due by an OutState Student
-	  * If a Student is part time(<12 credits), they pay 846 for University Fee
-	  * If a Student is full time(>=12 credits), they pay $1,441 for University Fee
-	  * Outstate students who are in TriState get a discount of $200/credit
+	  * Computes the total Tuition Due by an OutState Student
+	  * The Student pays $756/credit, regardless of enrollment type
+	  * More than 15 credits, counts as 15 credits when calculating TuitionDue
+	  * Part Time Student(<12 credits), pay $846 for University Fee
+	  * Full Time Student(>=12 credits), pay $1,441 for University Fee
+	  * TriState Student get a discount of $200/credit
 	  * @return total tuition amount due 
 	  */
 	public int tuitionDue() {
 		int tuitionDue; 
-		if(credit < 12)
-			tuitionDue = 846 + 756 * credit; 
-		else
-			tuitionDue = 1441 + 756 * credit; 
-		if(tristate) 
-			tuitionDue = tuitionDue - 200 * credit; 
 		
+		if(credit < 12) {
+				tuitionDue = 846 + 756 * credit; 
+		}
+		else if(credit < 15) {
+			if(tristate) 
+				tuitionDue = 1441 + (756 - 200) * credit;
+			else
+				tuitionDue = 1441 + 756 * credit; 
+		}
+		else {
+			if(tristate) 
+				tuitionDue = 1441 + (756-200) * 15;  
+			else
+				tuitionDue = 1441 + 756 * 15; 
+		}
 		return tuitionDue; 
+	}
+	
+	
+	 /**
+	  * toString() calls the superclass toString() method and adds it's own additional fields
+	  * @return string with name, last name, credit, tristate, tuitionDue 
+	  */
+	public String toString(){
+		return super.toString() + " " + tristate + " " + tuitionDue(); 
+	}
+	
+	
+	/**
+	 * The following is a test bed for Outstate Students
+	 * Each test invokes the toString() and tuitionDue() methods
+	 * The corresponding expected input/output can be found in the test doc 
+	 */
+	public static void main(String[] args) {
+		
+		Outstate student1 = new Outstate("Mary", "Anderson", 17, false);
+		System.out.println("Student 1 Entry: " + student1.toString()); 
+		System.out.println("Total Tuition Due: $" + student1.tuitionDue());
+		
+		Outstate student = new Outstate("Lauren", "Brown", 17, true);
+		System.out.println("Student 2 Entry: " + student.toString()); 
+		System.out.println("Total Tuition Due: $" + student.tuitionDue());
+		
+		Outstate student2 = new Outstate("Simons", "Michael", 8, false);
+		System.out.println("Student 3 Entry: " + student2.toString()); 
+		System.out.println("Total Tuition Due: $" + student2.tuitionDue());
+		
+		Outstate student3 = new Outstate("Stiller", "Anderson", 8, true);
+		System.out.println("Student 4 Entry: " + student3.toString()); 
+		System.out.println("Total Tuition Due: $" + student3.tuitionDue());
+		
+		Outstate student4 = new Outstate("Good", "Man", 12, false);
+		System.out.println("Student 5 Entry: " + student4.toString()); 
+		System.out.println("Total Tuition Due: $" + student4.tuitionDue());
+		
+		Outstate student5 = new Outstate("John", "White", 12, true);
+		System.out.println("Student 6 Entry: " + student5.toString()); 
+		System.out.println("Total Tuition Due: $" + student5.tuitionDue());
+	
 	}
 
 }
-
